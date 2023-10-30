646aaeed606799c256cd063aa8dfd41d495b45bc
danvornovitskii <danilafirerussia@gmail.com>
2023-10-30 21:00:28 +0800

    Initial commit

---
+++ RandomUsers/RandomUsersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomUsersApp.swift
+//  RandomUsers
+//
+//  Created by Danila Vornovitskii on 30.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct RandomUsersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
